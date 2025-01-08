d0bc19399f610b80bfff1af4906783a72171d20d
te22e-eriksson-adrian <adrian.eriksson@elev.ga.ntig.se>
2025-01-08 11:18:28 +0100

    Gjorde klart fungerande kod till och med punkt 6 samt såg till att språket är i enlighet med koden.

--- src/ProgramMain.java
+++ src/ProgramMain.java
@@ -1,11 +1,26 @@
+import java.util.Scanner;
 import java.util.ArrayList;
 
 public class ProgramMain {
     private ArrayList<Book> books = new ArrayList<>();
+    private ArrayList<Rock> rockList = new ArrayList<>();
+    Scanner keyboard = new Scanner(System.in);
 
     public ProgramMain(){
         //Instanser i början.
         createInfo();
+
+        System.out.println();
+        System.out.print("How many rocks would you like to create?: ");
+        int amountOfRocks = keyboard.nextInt();
+        keyboard.nextLine();
+
+        for (int i = 0; i < amountOfRocks; i++){
+            System.out.print("How much should rock "+(i + 1)+" weigh (kg)?: ");
+            int weight = keyboard.nextInt();
+            keyboard.nextLine();
+            rockList.add(new Rock(weight));
+        }
     }
     public void createInfo(){
         books.add(new Book("Exploring lost worlds", 235));  //Varför fungerar inte detta?
---
+++ src/Rock.java
@@ -0,0 +1,10 @@
+public class Rock {
+    private int weight;
+
+    public Rock(int w){     // w = weight
+        this.weight = w;
+    }
+    public int getWeight(){
+        return weight;
+    }
+}
